e0f9dc72bbac423c4235b75013ae3c74f87eba83
Joshua Moses <mosesjoshua350@gmail.com>
2021-04-27 13:30:49 +0100

    final commit

--- myblog/config/settings.py
+++ myblog/config/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT  =   [str(BASE_DIR.joinpath('static'))]
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 
